882645b6014e7532f874823a565eafd05473f145
Wilson Salles Junior <64506476+WilsonSalles-Jnr@users.noreply.github.com>
2022-05-24 23:18:25 -0300

    feat:  contexto da tabela criado

---
+++ Sapataria-de-Sapatos/Models/contexto.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Sapataria_de_Sapatos.Models
+{
+    public class contexto: DbContext
+    {
+        public contexto(DbContextOptions<contexto> options): base(options) { }
+        public DbSet<produto>? Produtos { get; set; }
+    }
+}
