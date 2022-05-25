2d1c0e82c161102e78991f02bf7680aabf6b047e
Wilson Salles Junior <64506476+WilsonSalles-Jnr@users.noreply.github.com>
2022-05-24 23:17:01 -0300

    feat: inserindo model de todas as tabelas

--- Sapataria-de-Sapatos/Models/produto.cs
+++ Sapataria-de-Sapatos/Models/produto.cs
@@ -1,12 +1,83 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Sapataria_de_Sapatos.Models
 {
     public class produto
     {
+        [Key][DatabaseGenerated(DatabaseGeneratedOption.Identity)][Required]
+        public int Id { get; set; }
+        [Required]
+        public string? Nome { get; set; }
+        [Required]
+        public decimal Preco { get; set; }
+    }
+    public class genero
+    {
+        [Key][DatabaseGenerated(DatabaseGeneratedOption.Identity)][Required]
+        public int Id { get; set; }
+        [Required]
+        public string? Genero { get; set; }
+    }
+    public class admin
+    {
+        [Key][DatabaseGenerated(DatabaseGeneratedOption.Identity)][Required]
+        public int Id { get; set; }
+        [Required]
+        public string? Gerente { get; set; }
+        [Required]
+        public string? token { get; set; }
+    }
+    public class loja
+    {
         [Key]
-        public int id { get; set; }
-        public string nome { get; set; }
-        public int preco { get; set; }
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Required]
+        public int Id { get; set; }
+        [Required]
+        public string? Nome { get; set; }
+        [Required]
+        public string? Cidade { get; set; }
+        [Required]
+        public string? Estado { get; set; }
+    }
+    public class vendedor
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Required]
+        public int Id { get; set; }
+        [Required]
+        public string? Nome { get; set; }
+        [Required]
+        public int Idade { get; set; }
+        [Required]
+        public List<genero>? GeneroId { get; set; }
+        [Required]
+        public int MesInicio { get; set; }
+        [Required]
+        public int AnoInicio { get; set; }
+    }
+    public class cliente
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Required]
+        public int Id { get; set; }
+        [Required]
+        public string? Nome { get; set; }
+        [Required]
+        public int Idade { get; set; }
+        [Required]
+        public List<genero>? GeneroId { get; set; }
+    }
+    public class vendas
+    {
+        [Required]
+        public List<produto>? produtoId { get; set; }
+        [Required]
+        public List<vendedor>? vendedorId { get; set; }
+        [Required]
+        public List<cliente>? clienteId { get; set; }
     }
 }
